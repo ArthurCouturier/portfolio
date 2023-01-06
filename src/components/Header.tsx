9acd66662fc3f0b846bbb764be751924934ddfe3
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2023-01-06 17:49:05 +0100

    updated href for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,14 +14,14 @@ export default function Header() {
                 !displayMenu? (
                     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 flex">
                         <div className={"flex-1"}>
-                            <p className="text-3xl text-gray-700 font-bold mb-5">
+                            <a className="text-3xl text-gray-700 font-extrabold mb-5" href={"/"}>
                                 CoutCout portfolio
-                            </p>
-                            <p className="text-gray-500 text-lg">
+                            </a>
+                            <div className="text-gray-500 text-lg">
                                 React and Tailwind CSS are in action on this website
-                            </p>
+                            </div>
                         </div>
-                        <div className={"flex-1 inset-y-0 right-0 relative"}>
+                        <div className={"flex-3 inset-y-0 right-0 relative"}>
                             <svg viewBox="0 0 100 80" className={"w-10 h-10 absolute inset-y-5 right-10"} onClick={changeDisplayMenu}>
                                 <rect className={"right-0"} width="100" height="10"></rect>
                                 <rect y="30" width="50" height="10"></rect>
@@ -32,14 +32,22 @@ export default function Header() {
                 ) : (
                     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 flex">
                         <div className={"flex-1"}>
-                            <p className="text-3xl text-gray-700 font-bold mb-5">
+                            <a className="text-3xl text-gray-700 font-extrabold mb-5" href={"/"}>
                                 Menu
-                            </p>
-                            <p className="text-gray-500 text-lg">
-                                This will be my menu
-                            </p>
+                            </a>
+                            <div className={"flex text-gray-500 text-lg"}>
+                                <a className={"flex-1"} href={"/Skills"}>
+                                    Skills
+                                </a>
+                                <a className={"flex-1"} href={"/Works"}>
+                                    Works
+                                </a>
+                                <a className={"flex-1"} href={"/About"}>
+                                    About
+                                </a>
+                            </div>
                         </div>
-                        <div className={"flex-1 inset-y-0 right-0 relative"}>
+                        <div className={"flex-3 inset-y-0 right-0 relative"}>
                             <svg viewBox="0 0 100 80" className={"w-10 h-10 absolute inset-y-5 right-10"} onClick={changeDisplayMenu}>
                                 <rect className={"right-0"} width="100" height="10"></rect>
                                 <rect y="30" width="50" height="10"></rect>
