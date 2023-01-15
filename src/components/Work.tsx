4e5f81668f459da3bf94f7a7e22dc366dc8710b4
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2023-01-15 19:18:03 +0100

    finished works

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,14 +1,14 @@
 import React, {useState} from "react";
-import {redirect} from "react-router-dom";
 
-export default function Work(props: { title: string, url: string }) {
-    const { title, url } = props;
+export default function Work(props: { title: string, url: string, desc?: string }) {
+    const { title, url, desc } = props;
 
     return (
         <div className={`text-center`}>
             <a className={"text-2xl font-extrabold mb-5 opacity-80 transition duration-300 hover:opacity-100 hover:underline"} href={url} target={"_blank"}>
                 {title}
             </a>
+            <div className={`text-gray-200`}>{desc}</div>
             <iframe className={`pointer-events-none my-7 w-[60vw] h-[60vh] mx-auto transition duration-1000`} src={url} />
         </div>
     );
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -7,6 +7,15 @@ export default function Works() {
             <div className={"h-max w-[100vw]"}>
                 <Work title={"Cave à vin"} url={"http://191.96.53.174"} />
                 <Work title={"This PortFolio"} url={"/"} />
+
+                <div className={"text-center"}>
+                    <a className={"text-2xl font-extrabold mb-5 opacity-80 transition duration-300 hover:opacity-100 hover:underline"} href={"https://github.com/ArthurCouturier/enseeiht"} target={"_blank"}>
+                        ENSEEIHT & Personnal Projects
+                    </a>
+                    <div className={`text-gray-200 my-4`}>
+                        Some projects I did. I will create special pages to illustrate it later.
+                    </div>
+                </div>
             </div>
         </div>
     )
