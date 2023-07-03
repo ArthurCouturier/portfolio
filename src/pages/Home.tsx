806fafc5a71468858ac7d85fe2172cae162b7195
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2023-07-03 18:25:28 +0200

    fixed bugs and updated resume

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as SkillsSVG } from "../assets/icons8-walter-white.svg";
 // @ts-ignore
 import { ReactComponent as WorksSVG } from "../assets/icons8-toolbox.svg";
 // @ts-ignore
-import pdf from '../assets/cvM1.pdf';
+import pdf from '../assets/cv-sortie-n7.pdf';
 // @ts-ignore
 import picture from '../assets/PhotoCouturierArthur.png';
 import InternRoute from "./InternRoute";
@@ -15,7 +15,7 @@ import InternRoute from "./InternRoute";
 export default function Home() {
     return (
         // bg-gray-200
-        <div className={"bg-animation bg-secondary bg-waves bg-center bg-cover animate-animateBG text-gray-200"} >
+        <div className={"bg-animation bg-black bg-secondary bg-waves bg-center bg-cover animate-animateBG text-gray-200"} >
             <div className={"flex-grow xl:text-3xl"}>
                 <div className={"h-[10vh]"}></div>
                 <div className={"md:h-[30vh] font-bold text-m sm:text-xl md:flex sm:h-[50vh] sm:flex-grow"}>
@@ -28,12 +28,12 @@ export default function Home() {
                     </div>
                     <div className={"flex-1 text-right mx-14 my-7 md:my-0"}>
                         <div>
-                            I study at
-                            <a href={"https://metropole.toulouse.fr"} target="_blank" className={"transition duration-500 hover:text-blue-800"}> ENSEEIHT </a>
+                            I studied at
+                            <a href={"https://www.enseeiht.fr/fr/index.html"} target="_blank" className={"transition duration-500 hover:text-blue-800"}> ENSEEIHT </a>
                             and
                         </div>
                         <div>
-                            work at
+                            now work at
                             <a href={"https://www.lyra.com/fr/"} target="_blank" className={"transition duration-500 hover:text-blue-400"}> Lyra Network</a>
                         </div>
                     </div>
